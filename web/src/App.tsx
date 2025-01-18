57726593e6b8b26de0580a9caf121c7cfb3b1dff
Hwang, JaeHyeon <ten02130735@gmail.com>
2025-01-18 22:28:24 +0900

    디자인 제외하고 전체 프엔 구조 작성

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import Header from "./component/Header.tsx";
+import Welcome from "./component/page/Welcome.tsx";
+import Resume from "./component/page/Resume.tsx";
+import Notice from "./component/page/Notice.tsx";
+import Footer from "./component/page/Footer.tsx";
 function App() {
   return (
-    <div>안녕하세요 React!</div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Welcome value="환영합니다!" color="blue" />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/notice" element={<Notice />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ web/src/component/page/Resume.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../scss/Resume.scss";
+
+function Resume() {
+  return (
+    <div className="resume">
+      <h1>Resume</h1>
+      <p>소개 화면 입니다.</p>
+    </div>
+  );
+}
+
+export default Resume;
